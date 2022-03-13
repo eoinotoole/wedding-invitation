b035ad57c5e3dd8091c1d931b4b4ab76e2882c88
Eoin O'Toole <eoinjamesotoole@gmail.com>
2022-03-13 11:41:39 +0000

    moved heavy svgs to inline approach

--- client/js/images.js
+++ client/js/images.js
@@ -1,7 +1,6 @@
-import leaves1 from "../images/leaves1.svg";
-import leaves2 from "../images/leaves2.svg";
-import leaves3 from "../images/leaves3.svg";
-import leaves4 from "../images/leaves4.svg";
-import leaves5 from "../images/leaves5.svg";
+window.addEventListener("load", handleWindowLoaded);
 
-export default { leaves1, leaves2, leaves3, leaves4, leaves5 };
+function handleWindowLoaded() {
+  const svgs = Array.from(document.querySelectorAll("svg"));
+  svgs.forEach((svg) => (svg.style.display = "block"));
+}

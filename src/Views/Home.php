ad4c7888ee3ddb882a3cbdc2a90652b6fb5d5c4a
Eoin O'Toole <eoinjamesotoole@gmail.com>
2022-03-13 12:39:36 +0000

    moved back to compressed svg files

--- src/Views/Home.php
+++ src/Views/Home.php
@@ -17,12 +17,8 @@ require_once __DIR__ . '/includes/header.php';
     </div> -->
     <main>
         <section class="intro">
-            <div class="intro__leaves">
-                <?php require_once __DIR__ . '/utils/svgs/leaves1.php';  ?>
-            </div>
-            <div class="intro__leaves-2">
-                <?php require_once __DIR__ . '/utils/svgs/leaves2.php'; ?>
-            </div>
+            <img class="intro__leaves" src="/static/images/leaves1.svg" />
+            <img class="intro__leaves-2" src="/static/images/leaves2.svg" />
             <div class="container">
                 <div class="intro__content">
                     <h5 class="intro__small">The wedding of</h5>
@@ -89,9 +85,7 @@ require_once __DIR__ . '/includes/header.php';
 
         <section class="accommodation">
             <div class="container">
-                <div class="accommodation__leaves">
-                    <?php require_once __DIR__ . '/utils/svgs/leaves5.php'; ?>
-                </div>
+                <img class="accommodation__leaves" src="/static/images/leaves5.svg" />
                 <h3 class="heading">Accommodation</h3>
                 <p>All wedding guests have a room reserved for the night of the wedding.
                     If a room is not required, please let us know asap.</p>
@@ -106,12 +100,8 @@ require_once __DIR__ . '/includes/header.php';
         </section>
 
         <section class="venue">
-            <div class="venue__leaves-1">
-                <?php require_once __DIR__ . '/utils/svgs/leaves3.php'; ?>
-            </div>
-            <div class="venue__leaves-2">
-                <?php require_once __DIR__ . '/utils/svgs/leaves4.php'; ?>
-            </div>
+            <img class="venue__leaves-1" src="/static/images/leaves3.svg" />
+            <img class="venue__leaves-2" src="/static/images/leaves4.svg" />
             <div class="container">
                 <h3 class="heading">About the venue</h3>
                 <p>See website:

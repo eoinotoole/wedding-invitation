978dab5d6015add563fa8004bf7a14b7c328b65f
Eoin O'Toole <eoinjamesotoole@gmail.com>
2022-06-21 10:01:06 +0100

    added new images

--- client/src/js/images.js
+++ client/src/js/images.js
@@ -1,8 +1,6 @@
-import leaves1 from "../images/leaves1.svg";
-import leaves2 from "../images/leaves2.svg";
-import leaves3 from "../images/leaves3.svg";
-import leaves4 from "../images/leaves4.svg";
-import leaves5 from "../images/leaves5.svg";
+import leaves1 from "../images/new-leaves1.png";
+import leaves2 from "../images/new-leaves2.png";
+import leaves3 from "../images/new-leaves3.png";
 
 import menuIcon from "../images/icon-menu.svg";
 import closeIcon from "../images/icon-close.svg";
@@ -11,8 +9,6 @@ export default {
   leaves1,
   leaves2,
   leaves3,
-  leaves4,
-  leaves5,
   menuIcon,
   closeIcon,
 };
